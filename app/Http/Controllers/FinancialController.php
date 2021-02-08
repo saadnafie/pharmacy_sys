9a419e7cca2a69e6abdcfed976117b5f87e5e399
saad@easy-trademarks.com <saadnafie@gmail.com>
2021-02-08 17:36:04 +0200

    Manual Account Entry

--- app/Http/Controllers/FinancialController.php
+++ app/Http/Controllers/FinancialController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
 
 use App\Models\TreeAccount;
 use App\Models\FinalAccount;
+use App\Models\AccountingEntry;
+use App\Models\EntryAction;
 
 class FinancialController extends Controller
 {
@@ -51,5 +54,48 @@ class FinancialController extends Controller
         $account->save();
 
         return redirect()->back();
+    }
+
+
+    ////////////////////////// Entries ///////////////////////////////////
+
+    public function show_all_entries(){
+        $entries = AccountingEntry::with('acctions')->get();
+        return view('admin.financial.dailyentrylist',compact('entries'));
+    }
+
+    public function add_entry_page(){
+        $tree_accounts = TreeAccount::where('account_type',1)->get();
+        return view('admin.financial.dailyentry',compact('tree_accounts'));
+    }
+
+    public function add_new_entry(Request $request){
+        //return $request;
+        $entry = new AccountingEntry();
+        $entry->type = 0;
+        $entry->title_en = "Manual";
+        $entry->title_ar = "يدوي";
+        $entry->date = $request->ent_date;
+        $entry->description = $request->ent_description;
+        $entry->user_id = auth()->user()->id;
+        $entry->save();
+        foreach ($request->accounts as $key => $acc) {
+            $account = TreeAccount::find($acc);
+            $account->balance = $account->balance - ($request->credit)[$key];
+            $account->balance = $account->balance + ($request->debit)[$key];
+            $account->save();
+
+            $action = new EntryAction();
+            $action->entry_id = $entry->id;
+            $action->tree_id = $acc;
+            $action->credit = ($request->credit)[$key];
+            $action->debit = ($request->debit)[$key];
+            $action->balance = $account->balance;
+            $action->description = ($request->description)[$key];
+            $action->save();
+        }
+
+        Session::flash('success', 'تمت العملية بنجاح!');
+        return redirect()->route('dailyentrylist');
     }
 }
